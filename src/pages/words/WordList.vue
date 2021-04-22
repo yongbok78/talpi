337c16a10ea7878adaf6f804081e4063a137d6d7
Yong Bok Ko <kyb01179@gmail.com>
2021-04-22 22:42:49 +0900

    단어변경 로직 수정 중

--- src/pages/words/WordList.vue
+++ src/pages/words/WordList.vue
@@ -1,8 +1,7 @@
 <template>
   <q-toolbar class="bg-primary glossy">
     <q-select v-model="book" :options="books" label="책"></q-select>
-    <q-select v-model="step" :options="book.steps" label="단계"></q-select>
-    <q-select v-model="book" :options="books" label="세부단계"></q-select>
+    <q-select v-model="step" :options="steps" label="단계"></q-select>
     <q-select v-model="difficulty" :options="difficultys" label="난이도"></q-select>
   </q-toolbar>
   <div class="q-ma-sm row justify-center">
@@ -61,7 +60,7 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import { throttle } from "quasar";
 import XLSX from "xlsx";
 import db from "../../common/db";
@@ -69,23 +68,27 @@ import { useQuasar } from "quasar";
 
 export default {
   setup() {
-    useQuasar().dark.set(true);
-
-    const virtualListRef = ref(null);
-    const virtualListIndex = ref(-1);
-
-    const wordList = ref([]);
-    onMounted(() => {
-      db.words.toArray((arr) => (wordList.value = arr));
-    });
-
     const books = [
       { label: "초급편", value: "beginner" },
       {
         label: "초급편2",
         value: "beginner2",
-        steps: [{ label: "1단계" }, { label: "2단계" }],
       },
+    ];
+    const steps = [
+      { label: "1-1", value: "1-1" },
+      { label: "1-2", value: "1-2" },
+      { label: "1-3", value: "1-3" },
+      { label: "1-4", value: "1-4" },
+      { label: "1-5", value: "1-5" },
+      { label: "1-6", value: "1-6" },
+      { label: "2-1", value: "2-1" },
+      { label: "2-2", value: "2-2" },
+      { label: "2-2a", value: "2-2a" },
+      { label: "k-e", value: "k-e" },
+      { label: "2-3", value: "2-3" },
+      { label: "2-4", value: "2-4" },
+      { label: "2-5", value: "2-5" },
     ];
     const difficultys = [
       { label: "전체", value: 0 },
@@ -94,33 +97,54 @@ export default {
       { label: "핵심", value: 3 },
     ];
     const book = ref(books[1]);
+    const step = ref(steps[3]);
     const difficulty = ref(difficultys[1]);
-    const step = ref(books[1].steps[0]);
+
+    useQuasar().dark.set(true);
+
+    const virtualListRef = ref(null);
+    const virtualListIndex = ref(38);
+
+    const wordList = ref([]);
+    onMounted(() => {
+      db.words.toArray((arr) => (wordList.value = arr));
+    });
+
     const played = ref(false);
     const play = throttle(() => {
       played.value = !played.value;
       changeWord();
     }, 3000);
-    const changeWord = () => {
-      if (virtualListIndex.value === wordList.value.length) {
-        played.value = false;
-        virtualListIndex.value = -1;
-        virtualListRef.value.scrollTo(0);
-      }
-      if (played.value) {
-        virtualListRef.value.scrollTo(++virtualListIndex.value, "center-force");
-        const ad = new Audio(
-          "/mp3/beginner2/beginner2_" +
-            wordList.value[virtualListIndex.value].id.toString().padStart(3, "0") +
-            ".mp3"
-        );
-        ad.addEventListener("ended", (event) => {
-          setTimeout(() => {
-            changeWord();
-          }, 1500);
+
+    let playInterval = 0;
+    watch(played, (played) => {
+      if (played) {
+        tellWord(wordList[virtualListIndex.value].id).then(() => {
+          playInterval = setInterval(() => {}, 1500);
         });
-        ad.play();
+      } else {
+        clearInterval(playInterval);
       }
+    });
+    const tellWord = (id) => {
+      return new Promise((res, rej) => {
+        try {
+          const ad = new Audio(
+            `/mp3/${book.value.value}/${book.value.value}_${id
+              .toString()
+              .padStart(3, "0")}.mp3`
+          );
+
+          ad.addEventListener("ended", (event) => {
+            setTimeout(() => {
+              res();
+            }, 1500);
+          });
+          ad.play();
+        } catch (err) {
+          rej();
+        }
+      });
     };
 
     const stopMinutes = ref(25);
@@ -164,8 +188,9 @@ export default {
 
     return {
       book,
-      step,
       books,
+      step,
+      steps,
       difficulty,
       difficultys,
       played,
